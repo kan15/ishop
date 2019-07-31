0e7b4b0a751ad388d9456df5871d19123e947b75
Oleg Kan <kan.fcsd@gmail.com>
2019-07-31 21:18:59 +0300

    added dashboard

---
+++ dashboard.js
@@ -0,0 +1,21 @@
+Vue.component('dashboard',{
+    template:`
+    <div class='widgets-container'>
+        <div class="card" style="width: 40%">
+            <img src="https://picsum.photos/500/300" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Товары</h5>
+                <p class="card-text"><h5><span class="badge badge-secondary">100</span></h5></p>
+            </div>
+        </div>
+        <div class="card" style="width: 40%">
+        <img src="https://picsum.photos/500/300?a=345345" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">Товары</h5>
+            <p class="card-text"><h5><span class="badge badge-secondary">100</span></h5></p>
+        </div>
+    </div>
+    </div>
+    
+    `
+})
